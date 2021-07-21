ff2dde2b62cb4e0f6aa0c0c757cad0144b0f39f7
guenit <71244831+guenit@users.noreply.github.com>
2021-07-21 17:16:24 +0800

    fetch ksm rpc

--- script/fast_v2.rb
+++ script/fast_v2.rb
@@ -12,7 +12,6 @@ JS_APP_PATH = CURRENT_PATH.join("..", "vendor", "polkadot_js_snippets")
 CHAIN_ENDPOINTS = {
   dev: "http://127.0.0.1:9933", # "ws://127.0.0.1:9944"
   kusama: "https://kusama-rpc.polkadot.io" # "ws://127.0.0.1:9944"
-  # kusama: "http://127.0.0.1:9933" # "ws://127.0.0.1:9944"
 }.with_indifferent_access
 
 current_chain = ARGV[0]
@@ -20,25 +19,25 @@ unless CHAIN_ENDPOINTS[current_chain]
   raise "Unknown chain `#{current_chain}`"
 end
 
-start_block_num = State.start_block_num(chain: current_chain)
-current_block_num = State.current_block_num(chain: current_chain)
+start_block_num_v2 = State.start_block_num_v2(chain: current_chain)
+current_block_num_v2 = State.current_block_num_v2(chain: current_chain)
 
-if current_block_num.integer_value < start_block_num.integer_value
-  current_block_num.update_attribute :integer_value, start_block_num.integer_value
+if current_block_num_v2.integer_value < start_block_num_v2.integer_value
+  current_block_num_v2.update_attribute :integer_value, start_block_num_v2.integer_value
 end
 
 loop do
 
-  if current_block_num.integer_value > 8_299_000
-    pp "sync block done，blokc num is ", current_block_num.integer_value
+  if current_block_num_v2.integer_value > 8_399_000
+    pp "sync block done，blokc num is ", current_block_num_v2.integer_value
     break
   end
 
-  puts "Fetching chain `#{current_chain}` contribution events at block num #{current_block_num.integer_value}"
-  out = URI.open("http://127.0.0.1:3001/fetch_block/#{current_block_num.integer_value}").read rescue nil
+  puts "Fetching chain `#{current_chain}` contribution events at block num #{current_block_num_v2.integer_value}"
+  out = URI.open("http://127.0.0.1:3001/fetch_block/#{current_block_num_v2.integer_value}").read rescue nil
   if out.blank? || out == "{}"
     puts "No output, sleep 5s..."
-      # sleep 0.3
+    sleep 0.1
     next
   end
 
@@ -50,8 +49,8 @@ loop do
   timestamp = Time.at(raw_timestamp)
 
   fund_infos =
-    if current_block_num.integer_value % 10 == 0
-      o = URI.open("http://127.0.0.1:3001/fund_infos?block_number=#{current_block_num.integer_value}").read rescue nil
+    if current_block_num_v2.integer_value % 10 == 0
+      o = URI.open("http://127.0.0.1:3001/fund_infos?block_number=#{current_block_num_v2.integer_value}").read rescue nil
 
       JSON.parse o rescue nil
     end
@@ -60,11 +59,11 @@ loop do
   contribution_events = jsonify_out.fetch("contributions").map do |item|
     {
       chain: current_chain,
-      block_num: current_block_num.integer_value,
+      block_num: current_block_num_v2.integer_value,
       who: item.fetch("who"),
       fund_index: item.fetch("fund_index"),
       amount: item.fetch("amount").to_s.to_d,
-      on_chain_hash: current_block_num.integer_value,
+      on_chain_hash: current_block_num_v2.integer_value,
       timestamp: timestamp,
       created_at: Time.zone.now
     }
@@ -73,11 +72,11 @@ loop do
   remark_referrer_events = jsonify_out.fetch("referrer_remarks").map do |item|
     {
       chain: current_chain,
-      block_num: current_block_num.integer_value,
+      block_num: current_block_num_v2.integer_value,
       who: item.fetch("who"),
       para_id: item.fetch("para_id"),
       referrer: item.fetch("referrer"),
-      on_chain_hash: current_block_num.integer_value,
+      on_chain_hash: current_block_num_v2.integer_value,
       timestamp: timestamp,
       created_at: Time.zone.now
     }
@@ -97,7 +96,7 @@ loop do
     end
 
     Campaign
-      .where("start_block <= ? AND end_block >= ?", current_block_num.integer_value, current_block_num.integer_value)
+      .where("start_block <= ? AND end_block >= ?", current_block_num_v2.integer_value, current_block_num_v2.integer_value)
       .each do |campaign|
         nearest_hour = timestamp.beginning_of_hour
         nearest_hour = nearest_hour - (nearest_hour.hour % 4).hour
@@ -115,14 +114,14 @@ loop do
         # TODO: May not get the final state, but not to critical I think
         Competitor
           .where("parachain_ids LIKE '%#{k}%'")
-          .where("start_block <= ? AND end_block >= ?", current_block_num.integer_value, current_block_num.integer_value)
+          .where("start_block <= ? AND end_block >= ?", current_block_num_v2.integer_value, current_block_num_v2.integer_value)
           .each do |competitor|
             competitor.increment! :raised_amount, v.fetch("raised").to_s.to_d
           end
       end
     end
 
-    current_block_num.update_attribute :integer_value, current_block_num.integer_value + 1
+    current_block_num_v2.update_attribute :integer_value, current_block_num_v2.integer_value + 1
   end
 
   if remark_referrer_events.size > 0 || contribution_events.size > 0
